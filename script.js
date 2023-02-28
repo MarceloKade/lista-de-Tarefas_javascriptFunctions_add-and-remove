a97096dc20eb58b15e067a261b864ef445a3b4c5
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-02-28 17:03:09 -0300

    lista de tarefas com opção de adicionar e remover nova tarefa utilizando funções em javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+let listaTarefas = []; // Array que armazena as tarefas
+
+function adicionarTarefa() {
+    let novaTarefa = document.getElementById("nova-tarefa").value;
+    if (novaTarefa !== "") { // Verifica se o campo não está vazio
+        listaTarefas.push(novaTarefa); // Adiciona a tarefa ao array
+        atualizarLista(); // Atualiza a lista de tarefas
+        document.getElementById("nova-tarefa").value = ""; // Limpa o campo de texto
+    }
+}
+
+function removerTarefa(index) {
+    listaTarefas.splice(index, 1); // Remove a tarefa do array
+    atualizarLista(); // Atualiza a lista de tarefas
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("lista-de-tarefas");
+    lista.innerHTML = ""; // Limpa a lista de tarefas
+    for (let i = 0; i < listaTarefas.length; i++) {
+        let tarefa = listaTarefas[i];
+        let itemDiv = document.createElement("div");
+        itemDiv.className = "d-flex justify-content-between align-items-center list-group-item";
+        let itemLista = document.createElement("e");
+        itemLista.innerHTML = tarefa;
+        let botaoRemover = document.createElement("button");
+        botaoRemover.type = "button";
+        botaoRemover.className = "close";
+        botaoRemover.setAttribute("aria-label", "Close");
+        botaoRemover.onclick = function () {
+            removerTarefa(i);
+        };
+        botaoRemover.innerHTML = '<span aria-hidden="true">&times;</span>';
+        itemDiv.appendChild(itemLista);
+        itemDiv.appendChild(botaoRemover);
+        lista.appendChild(itemDiv);
+    }
+}
